e2ec36741b3596953ae7bf68200b8b5111a45958
jin <jin@crypt.sg>
2014-08-11 02:19:00 +0800

    added image collection view

--- imagebrowser/app/controllers/main_controller.rb
+++ imagebrowser/app/controllers/main_controller.rb
@@ -13,12 +13,37 @@ class MainController < UIViewController
     @query = rmq.append(UITextField, :query).focus.get
 
     rmq.append(UIButton, :submit_button).on(:touch) do |sender|
-      puts "submit button tapped"
+      search_for_images @query.text
     end
   end
 
   def init_nav
     self.title = 'Image Browser'
+  end
+
+  def search_for_images(query)
+    if query and !query.empty?
+      query = query.gsub(/\s/, '%20')
+      url = "https://secure.flickr.com/search/?q=#{query}&s=int"
+
+      rmq.animations.start_spinner
+
+      AFMotion::HTTP.get(url) do |result|
+        if html = result.body
+          images = html.scan(/src=\"(.+?\.jpg)\"/i).map do |m|
+            m.first
+          end
+
+          open_images_controller(images) if !images.empty?
+          rmq.animations.stop_spinner
+        end
+      end
+    end
+  end
+
+  def open_images_controller(images)
+    controller = ImagesController.new
+    self.navigationController.pushViewController(controller, animated:true)
   end
 
 end
